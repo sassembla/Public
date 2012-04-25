e360a4714e0c2355fd08fdf163243ed1c340e8b8
Toru Inoue <toru.inoue@kissaki.tv>
2012-04-26 02:37:19 +0900

    anywhereでcomment機能制作開始。　Twitter@anywhereの情報が少ない、、

--- htmlnize.sh
+++ htmlnize.sh
@@ -3,7 +3,10 @@
 echo "htmlnize start"
 rm rtfds.log
 
-##list up
+
+#	.rtfd	#
+
+#list up
 find . -type d -name \*.rtfd -print -maxdepth 1 >> rtfds.log
 
 #read file-path from each line
@@ -15,25 +18,57 @@ for line in $(< rtfds.log);do
 	path=$(echo "$line" | sed -e 's/.rtfd//')
 	path=$(echo "$path" | sed -e 's/.\///')
 	echo path is $path
-	pwd
+	
 	#create directry from target-name(without ".rtfd")
 	mkdir "$path"
 	mv "$line" "$path"
 
 	cd "$path"
 	
-	#create html from data
+	#create html from .rtfd
 	textutil -convert html "$path".rtfd
 
 	targetHtmlFile="$path".html
-#	cat $targetHtmlFile
 	
 	#replace "file:///somefile.jpg" to "somefile.jpg"
 	sed -i -e 's/file:\/\/\///g' $targetHtmlFile
 	
+	#delete dust..
 	rm "$path".html-e
 	cd ../
 done
+
+
+
+
+#	.rtf	#
+rm rtfs.log
+
+#list up
+find . -type f -name \*.rtf -print -maxdepth 1 >> rtfs.log
+
+#read file-path from each line
+for line in $(< rtfs.log);do
+	path=$(echo "$line" | sed -e 's/.rtf//')
+	path=$(echo "$path" | sed -e 's/.\///')
+	echo path is $path
+	
+	#create directry from target-name(without ".rtf")
+	mkdir "$path"
+	mv "$line" "$path"
+
+	cd "$path"
+	
+	#create html from .rtf
+	textutil -convert html "$path".rtf
+
+	targetHtmlFile="$path".html
+	
+	#delete dust..
+	rm "$path".html-e
+	cd ../
+done
+
 
 
 echo "htmlnize done"
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,14 @@
 console.log('This would be the main JS file.');
+
+var get_home_timeline = function(){
+  twttr.anywhere(function(twitter) {
+    twitter.User.current().homeTimeline().first(20).each(
+        function(status) {
+			alert("status	"+status);
+             console.log(status.text);
+        }
+    );
+  });
+}
+
+get_home_timeline();
